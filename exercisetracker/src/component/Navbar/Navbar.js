6bb8bf9b5ab6d6279a861d52a4844789d6dca3fd
aswinks88 <aswinks88@gmail.com>
2020-03-18 00:18:33 +1300

    added all the required functionality for exercise tracker

--- exercisetracker/src/component/Navbar/Navbar.js
+++ exercisetracker/src/component/Navbar/Navbar.js
@@ -6,7 +6,10 @@ export default class Navbar extends Component {
         return(
             <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
                 <Link to='/' className='navbar-brand'>Exercise Tracker</Link>
-                <div className='collapse navbar-collapse'>
+                {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+      </button> */}
+                <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
                     <ul className='navbar-nav mr-auto'>
                         <li className='navbar-item'>
                             <Link to='/' className='nav-link'>Exercises</Link>
