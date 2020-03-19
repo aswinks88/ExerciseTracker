0c4cb11ec080621bb364b14a7444d4a795e140d0
aswinks88 <aswinks88@gmail.com>
2020-03-20 01:32:39 +1300

    added user list view and functionality to delete user

--- exercisetracker/src/component/CreateUser/CreateUser.js
+++ exercisetracker/src/component/CreateUser/CreateUser.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+
 export default class CreateUser extends Component {
     constructor(props){
         super(props)
         this.onChangeUsername = this.onChangeUsername.bind(this)
+        // this.deleteUsers = this.deleteUsers.bind(this)
         this.onSubmit = this.onSubmit.bind(this);
         this.state = {
-            username: ''
+            username: '',
+            users: []
         };
     }
+    componentDidMount(){
+      axios.get('http://localhost:5000/users/')
+      .then(response => {
+        this.setState({
+          users: response.data
+        })
+        console.log(this.state.users)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+ 
     onChangeUsername(e) {
         this.setState({
           username: e.target.value
@@ -27,6 +42,23 @@ export default class CreateUser extends Component {
         this.setState({
           username: ''
         })
+        window.location.reload('/user')
+      }
+
+      deleteUsers(id){
+      axios.delete('http://localhost:5000/users/' + id )
+      .then(res => console.log(res.data))
+       this.setState({
+         users: this.state.users.filter(ul => ul._id !== id)
+       })
+      }
+      userList(){
+       return this.state.users.map(userls => {
+         return (<tr>
+         <td>{userls.username}</td>
+         <td key={userls._id}><button className='btn btn-danger' onClick={this.deleteUsers.bind(this, userls._id)}>Delete</button></td>
+       </tr>)
+       })
       }
     render(){
         return(
@@ -45,7 +77,22 @@ export default class CreateUser extends Component {
     <div className="form-group">
       <input type="submit" value="Create User" className="btn btn-primary" />
     </div>
+    
   </form>
+  <div>
+      <h3>User List</h3>
+      <table className='table'>
+        <thead className='thead-light'>
+          <tr>
+            <th>Username</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            {this.userList()}
+        </tbody>
+      </table>
+    </div>
 </div>
         )
     }
